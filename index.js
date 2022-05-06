908c5ddc4ffd586f50d0e0de152de0197f5460c1
Shivam <shivamsingh280501@gmail.com>
2022-05-06 12:25:21 +0530

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const express = require('express');
+const res = require('express/lib/response');
+const app = express()
+const path = require('path')
+
+app.use('/', express.static(path.resolve(__dirname,'./public')));
+
+app.get('/',(req,res)=>{
+  res.render("index.ejs")
+})
+
+const server = app.listen(8080)
+
 // node server which handle socket io connection
 //To listen the event, use socket.on(event_name,callback_function)
-const io = require('socket.io')(8000, {
+const io = require('socket.io')(server, {
     cors: {
       origin: '*',
     }
